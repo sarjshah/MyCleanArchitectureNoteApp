d8b0c04a08567b682bc73cd64e4d348ce031543b
sarjshah <sarju@novoda.com>
2022-04-25 14:52:21 +0100

    Create helper class to hold UI state.

---
+++ app/src/main/java/com/practice/mycleanarchitecturenoteapp/feature/note/presentation/notes/NotesState.kt
@@ -0,0 +1,11 @@
+package com.practice.mycleanarchitecturenoteapp.feature.note.presentation.notes
+
+import com.practice.mycleanarchitecturenoteapp.feature.note.domain.model.Note
+import com.practice.mycleanarchitecturenoteapp.feature.note.domain.util.NoteOrder
+import com.practice.mycleanarchitecturenoteapp.feature.note.domain.util.OrderType
+
+data class NotesState(
+    val notes: List<Note> = emptyList(),
+    val noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending),
+    val isOrderSectionVisible: Boolean = false
+)
