a18910c2eb42a8fb7642ff9355434e57483087a9
sarjshah <sarju@novoda.com>
2022-04-26 17:42:12 +0100

    Change classname to uppercase

--- app/src/main/java/com/practice/mycleanarchitecturenoteapp/feature/note/presentation/notes/NotesEvent.kt
+++ app/src/main/java/com/practice/mycleanarchitecturenoteapp/feature/note/presentation/notes/NotesEvent.kt
@@ -4,9 +4,9 @@ import com.practice.mycleanarchitecturenoteapp.feature.note.domain.model.Note
 import com.practice.mycleanarchitecturenoteapp.feature.note.domain.util.NoteOrder
 
 sealed class NotesEvent {
-    data class Order(val order: NoteOrder): NotesEvent()
+    data class Order(val noteOrder: NoteOrder): NotesEvent()
     data class DeleteNote(val note: Note): NotesEvent()
     object RestoreNote: NotesEvent()
-    object toggleOrderSection: NotesEvent()
+    object ToggleOrderSection: NotesEvent()
     object GoToAddNote: NotesEvent()
 }
